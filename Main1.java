29ea4e8a190ee60801fd7e08a2a62fbbb3e361dd
Mohammad Jafar Jafar <jafarkashify@gmail.com>
2024-10-20 00:30:25 +0330

    Main1 class code added

--- Main1.java
+++ Main1.java
@@ -1,1 +1,14 @@
-public class 
+public class Main1 {
+    public static void main(String[] args) {
+        Lab lab = new Lab("MR Smith", "Monday", 30);
+        Student student = new Student("James", "Gosling", "0987854");
+        Student student1 = new Student("Dennis", "Richie", "1234567");
+
+        student.setGrade(18.0);
+        student1.setGrade(17.5);
+
+        student.printStudentInfo(student);
+        student1.printStudentInfo(student1);
+        lab.printLabInfo((18.0 + 17.5) / 2 ,lab, 2);
+    }
+}
