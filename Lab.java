5f7cb6645dbbd34e70c87a80b9f922198342d537
Mohammad Jafar Jafar <jafarkashify@gmail.com>
2024-10-19 16:54:55 +0330

    Lab class constructor, setters and getters

--- Lab.java
+++ Lab.java
@@ -6,5 +6,63 @@ public class Lab {
     private int maxSize;
     private int currentSize;
     private double avgGrade;
-    
+
+    // Constructor
+    public Lab(String teacherName, String dayOfWeek, int maxSize) {
+        this.teacherName = teacherName;
+        this.dayOfWeek = dayOfWeek;
+        this.maxSize = maxSize;
+        // we make an array in size of maxsize
+        // every roo of the array represent a student
+        students = new Student[maxSize];
+    }
+    // getters
+    // setters
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public void setStudents(Student[] students) {
+        this.students = students;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+    public String getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(String dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public int getCurrentSize() {
+        return currentSize;
+    }
+
+    public void setCurrentSize(int currentSize) {
+        this.currentSize = currentSize;
+    }
+
+    public double getAvgGrade() {
+        return avgGrade;
+    }
+
+    public void setAvgGrade(double avgGrade) {
+        this.avgGrade = avgGrade;
+    }
 }
