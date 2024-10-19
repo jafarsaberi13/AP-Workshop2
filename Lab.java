8810994af9fbfe8369d51dea87c2bdf719fcfa46
Mohammad Jafar Jafar <jafarkashify@gmail.com>
2024-10-19 16:43:38 +0330

    Lab class added

---
+++ Lab.java
@@ -0,0 +1,9 @@
+public class Lab {
+    private Student[] students;
+    private String teacherName;
+    // The day that class is held
+    private String dayOfWeek;
+    private int maxSize;
+    private int currentSize;
+    
+}
